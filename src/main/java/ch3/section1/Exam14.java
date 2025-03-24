5740ad26a4f3b31acdc15a36a773b7d3ce933c3d
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-03-24 10:13:25 +0900

    chapter3-section1

---
+++ src/main/java/ch3/section1/Exam14.java
@@ -0,0 +1,39 @@
+package ch3.section1;
+
+// 공통 코드 개발자가 작성했다고 가정
+class Employeeee {
+    String name;
+    int salary = 100;
+
+    int calcBonus() {
+        System.out.println("Employeeee 보너스 계산기");
+        return salary * 12;
+    }
+}
+
+class Calc {
+    public static void calcBonus(Employeeee e) {
+        System.out.println(e.calcBonus());
+    }
+}
+
+class Salesman extends Employeeee {
+    @Override
+    int calcBonus() {
+        System.out.println("Salesman 보너스 계산");
+        return salary * 12 * 4;
+    }
+}
+
+class Consultant extends Employeeee {
+    @Override
+    int calcBonus() {
+        System.out.println("Consultant 보너스 계산");
+        return salary * 12 * 2;
+    }
+}
+public class Exam14 {
+    public static void main(String[] args) {
+        Calc.calcBonus(new Salesman());
+    }
+}
